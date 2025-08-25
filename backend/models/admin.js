d784493e071b1c6213bc10594506e1181183c3d2
NehalArora <nehal.arora@cumminscollge.in>
2025-08-25 10:07:29 +0530

    added admin model

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-    studentId: { type: String, required: true, unique: true }, 
+    adminId: { type: String, required: true, unique: true }, 
     email: { type: String, required: true, unique: true },
     phone: { type: String, required: true },
-    college: { type: String, required: true },
-    year: { type: Number, required: true },
     password:{ type: String, required: true },
-    fieldOfStudy: { type: String, required: true },
     registeredEventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event' }, // Foreign Key to Events
     createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model('Student', studentSchema);
+module.exports = mongoose.model('Admin', adminSchema);
